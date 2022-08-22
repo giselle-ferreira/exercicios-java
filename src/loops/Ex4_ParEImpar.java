5da6f9abcdb025171560f6d3ae3e6e7c5668132e
giselle-ferreira <giselle.fs@gmail.com>
2022-08-21 21:32:30 -0300

    Adds lesson 4

---
+++ src/loops/Ex4_ParEImpar.java
@@ -0,0 +1,39 @@
+package loops;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números ímpares.
+ */
+
+import java.util.Scanner;
+
+public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0;
+        int quantImpares = 0;
+
+        System.out.println("Informe uma quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if(numero % 2 == 0) quantPares++;
+            else quantImpares++;
+
+            count++;
+
+        } while(count < quantNumeros);
+
+        System.out.println("Quantidade de números informados: " + quantNumeros);
+        System.out.println("Quantidade de Pares: " + quantPares);
+        System.out.println("Quantidade de Ímpares: " + quantImpares);
+    }
+}
